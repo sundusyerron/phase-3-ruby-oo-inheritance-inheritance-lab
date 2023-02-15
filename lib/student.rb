3707073d920b1d9e91a3b8e4dd1683e23dfeb8be
Sundus <sundusaden105@gmail.com>
2023-02-15 09:43:42 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
-
+    class Student < User
+            
+      def initialize
+        super
+        @knowledge = []
+      end
+    
+      def learn(string)
+        @knowledge << string
+      end
+      def knowledge
+        @knowledge
+        end
 end
